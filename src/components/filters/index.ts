2a200ad93160018e9c1445d4dbb2c30a67450340
Myshko Alona <myshko.alona@gmail.com>
2026-02-01 21:05:01 +0000

    update index

--- src/components/filters/index.ts
+++ src/components/filters/index.ts
@@ -4,7 +4,6 @@ import FiltersButton from "./FiltersButton";
 import LocationFilter from "./LocationFilter";
 import EquipmentFilters from "./EquipmentFilters";
 import TypeFilters from "./TypeFilters";
-
 export {
   FilterContent,
   FiltersDrawer,
